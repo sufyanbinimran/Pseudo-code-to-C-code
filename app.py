b92bea965e28d11cae3ea6ad50bf074c16edeb9a
Sufyan Malik <158352676+sufyanbinimran@users.noreply.github.com>
2025-02-27 19:42:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,174 @@
-import streamlit as st
-import pickle
 import tensorflow as tf
-import numpy as np
 from tensorflow import keras
-from transformer import Transformer  # Ensure 'transformer.py' contains the Transformer class
+from tensorflow.keras.layers import Layer, Embedding, Dense, Dropout, LayerNormalization
+import numpy as np
 
-# Register the Transformer class
-custom_objects = {"Transformer": Transformer}
+# Register Transformer Model
+@keras.saving.register_keras_serializable()
+class Transformer(keras.Model):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, target_vocab_size, max_pos_encoding, rate=0.1):
+        super(Transformer, self).__init__()
 
-# Load Tokenizers
-with open("tokenizer_cpp.pkl", "rb") as f:
-    tokenizer_cpp = pickle.load(f)
+        self.encoder = Encoder(num_layers, d_model, num_heads, dff, input_vocab_size, max_pos_encoding, rate)
+        self.decoder = Decoder(num_layers, d_model, num_heads, dff, target_vocab_size, max_pos_encoding, rate)
+        self.final_layer = Dense(target_vocab_size)
 
-with open("tokenizer_pseudo_to_cpp.pkl", "rb") as f:
-    tokenizer_pseudo = pickle.load(f)
+    def call(self, inputs, training=False):
+        inp, tar = inputs
+        enc_output = self.encoder(inp, training)
+        dec_output = self.decoder(tar, enc_output, training)
+        final_output = self.final_layer(dec_output)
+        return final_output
 
-# Load Transformer Model
-model = keras.models.load_model("transformer_pseudo_cpp.keras", custom_objects=custom_objects)
+# Encoder
+@keras.saving.register_keras_serializable()
+class Encoder(keras.layers.Layer):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, max_pos_encoding, rate=0.1):
+        super(Encoder, self).__init__()
 
-# Function to Convert Pseudocode to C++
-def predict_cpp(pseudo_code):
-    input_seq = tokenizer_pseudo.texts_to_sequences([pseudo_code])
-    input_seq = tf.keras.preprocessing.sequence.pad_sequences(input_seq, maxlen=100, padding="post")
-    
-    prediction = model.predict(input_seq)
-    predicted_tokens = np.argmax(prediction, axis=-1)
-    
-    cpp_code = tokenizer_cpp.sequences_to_texts(predicted_tokens)[0]
-    return cpp_code
+        self.embedding = Embedding(input_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(max_pos_encoding, d_model)
+        self.enc_layers = [EncoderLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        self.dropout = Dropout(rate)
 
-# Streamlit UI
-st.title("Pseudocode to C++ Converter")
-pseudo_code = st.text_area("Enter Pseudocode:")
+    def call(self, x, training):
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.embedding.output_dim, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+        x = self.dropout(x, training=training)
 
-if st.button("Convert"):
-    if pseudo_code:
-        cpp_code = predict_cpp(pseudo_code)
-        st.code(cpp_code, language="cpp")
-    else:
-        st.warning("Please enter some pseudocode.")
+        for enc_layer in self.enc_layers:
+            x = enc_layer(x, training)
+
+        return x
+
+# Decoder
+@keras.saving.register_keras_serializable()
+class Decoder(keras.layers.Layer):
+    def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size, max_pos_encoding, rate=0.1):
+        super(Decoder, self).__init__()
+
+        self.embedding = Embedding(target_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(max_pos_encoding, d_model)
+        self.dec_layers = [DecoderLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        self.dropout = Dropout(rate)
+
+    def call(self, x, enc_output, training):
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.embedding.output_dim, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+        x = self.dropout(x, training=training)
+
+        for dec_layer in self.dec_layers:
+            x = dec_layer(x, enc_output, training)
+
+        return x
+
+# Encoder Layer
+@keras.saving.register_keras_serializable()
+class EncoderLayer(keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+        self.dropout1 = Dropout(rate)
+        self.dropout2 = Dropout(rate)
+
+    def call(self, x, training):
+        attn_output = self.mha(x, x, x)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)
+
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(out1 + ffn_output)
+
+        return out2
+
+# Decoder Layer
+@keras.saving.register_keras_serializable()
+class DecoderLayer(keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(DecoderLayer, self).__init__()
+
+        self.mha1 = MultiHeadAttention(d_model, num_heads)
+        self.mha2 = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = Dropout(rate)
+        self.dropout2 = Dropout(rate)
+        self.dropout3 = Dropout(rate)
+
+    def call(self, x, enc_output, training):
+        attn1 = self.mha1(x, x, x)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(x + attn1)
+
+        attn2 = self.mha2(out1, enc_output, enc_output)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(out1 + attn2)
+
+        ffn_output = self.ffn(out2)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        out3 = self.layernorm3(out2 + ffn_output)
+
+        return out3
+
+# Multi-Head Attention
+@keras.saving.register_keras_serializable()
+class MultiHeadAttention(Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+
+        assert d_model % num_heads == 0
+        self.num_heads = num_heads
+        self.depth = d_model // num_heads
+
+        self.wq = Dense(d_model)
+        self.wk = Dense(d_model)
+        self.wv = Dense(d_model)
+
+        self.dense = Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, q, k, v):
+        batch_size = tf.shape(q)[0]
+
+        q = self.split_heads(self.wq(q), batch_size)
+        k = self.split_heads(self.wk(k), batch_size)
+        v = self.split_heads(self.wv(v), batch_size)
+
+        scaled_attention = tf.nn.softmax(tf.matmul(q, k, transpose_b=True) / tf.math.sqrt(tf.cast(self.depth, tf.float32)), axis=-1)
+        output = tf.matmul(scaled_attention, v)
+        output = tf.transpose(output, perm=[0, 2, 1, 3])
+        output = tf.reshape(output, (batch_size, -1, self.num_heads * self.depth))
+
+        return self.dense(output)
+
+# Feed Forward Network
+def point_wise_feed_forward_network(d_model, dff):
+    return keras.Sequential([
+        Dense(dff, activation="relu"),
+        Dense(d_model)
+    ])
+
+# Positional Encoding
+def positional_encoding(position, d_model):
+    angle_rads = np.arange(position)[:, np.newaxis] / np.power(10000, (2 * (np.arange(d_model)[np.newaxis, :] // 2)) / np.float32(d_model))
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return tf.cast(pos_encoding, dtype=tf.float32)
