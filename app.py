cd053a5a29e7c669733bd2735e44452eb5d48be9
Sufyan Malik <158352676+sufyanbinimran@users.noreply.github.com>
2025-03-15 15:19:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import streamlit as st
 import pickle
 import numpy as np
 
+@tf.keras.utils.register_keras_serializable()
 class MultiHeadAttention(tf.keras.layers.Layer):
     def __init__(self, d_model, num_heads):
         super(MultiHeadAttention, self).__init__()
         self.num_heads = num_heads
         self.d_model = d_model
-        assert d_model % num_heads == 0
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
         self.depth = d_model // num_heads
         self.wq = tf.keras.layers.Dense(d_model)
         self.wk = tf.keras.layers.Dense(d_model)
@@ -24,11 +25,16 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         q = self.split_heads(self.wq(q), batch_size)
         k = self.split_heads(self.wk(k), batch_size)
         v = self.split_heads(self.wv(v), batch_size)
-        attn_output = tf.nn.softmax(tf.matmul(q, k, transpose_b=True) / tf.math.sqrt(tf.cast(self.depth, tf.float32)))
+        # Scaled dot-product attention
+        attn_output = tf.nn.softmax(
+            tf.matmul(q, k, transpose_b=True) / tf.math.sqrt(tf.cast(self.depth, tf.float32))
+        )
         attn_output = tf.matmul(attn_output, v)
         attn_output = tf.transpose(attn_output, perm=[0, 2, 1, 3])
         return self.dense(tf.reshape(attn_output, (batch_size, -1, self.d_model)))
 
+
+@tf.keras.utils.register_keras_serializable()
 class TransformerBlock(tf.keras.layers.Layer):
     def __init__(self, d_model, num_heads, dff):
         super(TransformerBlock, self).__init__()
@@ -46,11 +52,13 @@ class TransformerBlock(tf.keras.layers.Layer):
         ffn_output = self.ffn(out1)
         return self.layernorm2(out1 + ffn_output)
 
+
 @tf.keras.utils.register_keras_serializable()
 class Transformer(tf.keras.Model):
     def __init__(self, vocab_size, d_model, num_heads, dff, max_len):
         super(Transformer, self).__init__()
-        self.embedding = tf.keras.layers.Embedding(vocab_size, d_model, input_length=max_len)
+        # Removed deprecated input_length
+        self.embedding = tf.keras.layers.Embedding(vocab_size, d_model)
         self.encoder = TransformerBlock(d_model, num_heads, dff)
         self.decoder = TransformerBlock(d_model, num_heads, dff)
         self.final_layer = tf.keras.layers.Dense(vocab_size, activation='softmax')
@@ -60,10 +68,17 @@ class Transformer(tf.keras.Model):
         dec_output = self.decoder(self.embedding(inputs))
         return self.final_layer(dec_output)
 
+
 @st.cache_resource
 def load_models():
-    model1 = tf.keras.models.load_model("transformer_model1.keras", safe_mode=False)
-    model2 = tf.keras.models.load_model("transformer_model2.keras", safe_mode=False)
+    # Define custom objects for loading the models
+    custom_objs = {
+        "Transformer": Transformer,
+        "TransformerBlock": TransformerBlock,
+        "MultiHeadAttention": MultiHeadAttention
+    }
+    model1 = tf.keras.models.load_model("transformer_model1.keras", safe_mode=False, custom_objects=custom_objs)
+    model2 = tf.keras.models.load_model("transformer_model2.keras", safe_mode=False, custom_objects=custom_objs)
     return model1, model2
 
 
@@ -79,8 +94,10 @@ def load_tokenizers():
         pseudocode_tokenizer2 = pickle.load(f)
     return cpp_tokenizer1, pseudocode_tokenizer1, cpp_tokenizer2, pseudocode_tokenizer2
 
+# Load models and tokenizers
 model1, model2 = load_models()
 cpp_tokenizer1, pseudocode_tokenizer1, cpp_tokenizer2, pseudocode_tokenizer2 = load_tokenizers()
+
 
 def convert_text(input_text, model, input_tokenizer, output_tokenizer):
     input_seq = input_tokenizer.texts_to_sequences([input_text])
@@ -89,6 +106,7 @@ def convert_text(input_text, model, input_tokenizer, output_tokenizer):
     predicted_seq = np.argmax(prediction, axis=-1)
     output_text = output_tokenizer.sequences_to_texts(predicted_seq)
     return output_text[0]
+
 
 st.title("Code Converter (Transformer Model)")
 option = st.selectbox("Select conversion type:", ["C++ to Pseudocode", "Pseudocode to C++"])
